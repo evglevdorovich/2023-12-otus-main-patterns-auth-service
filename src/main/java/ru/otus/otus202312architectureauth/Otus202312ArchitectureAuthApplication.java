096d37de9ea575f9a1e4c4bfaae87e2b8785d1d7
evglevdorovich <lev_jack@mail.ru>
2024-03-19 21:49:45 +0100

    init commit

---
+++ src/main/java/ru/otus/otus202312architectureauth/Otus202312ArchitectureAuthApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.otus202312architectureauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Otus202312ArchitectureAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Otus202312ArchitectureAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/otus202312architectureauth/Otus202312ArchitectureAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.otus202312architectureauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Otus202312ArchitectureAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
